abf1f0c78f223fe3b8bdf7fcc7f1e537c9b553af
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2021-10-17 22:10:54 +0700

    build layout and add styles for homepage slider component

--- src/components/SliderHomepage.jsx
+++ src/components/SliderHomepage.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
-import './SliderHomepage.css'
+import React from "react";
+import "./SliderHomepage.css";
+import colorSneaker from "../images/ColoredSneaker.png";
+import sliderThumb3 from "../images/sliderThumb3.png";
+import instagram from "../images/instagram.png";
+import twitter from "../images/twitter.png";
+import facebook from "../images/facebook.png";
 
 const SliderHomepage = () => {
-    return (
-        <div className="Test">
-
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="textBox">
+          <p className="above-heading-text">Women's Originals</p>
+          <h2>
+            Falcon Sneaker
+          </h2>
+          <p className="description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Non diam
+            phasellus vestibulum lorem.
+          </p>
+          <a href="#">Shop Now</a>
         </div>
-    )
-}
+        <div className="imgBox">
+          <img src={colorSneaker} alt="sneaker" className="sneakers" />
+        </div>
+      </div>
 
-export default SliderHomepage
+      <ul className="thumb">
+        <li>
+          <img src={sliderThumb3} alt="" />
+        </li>
+        <li>
+          <img src={sliderThumb3} alt="" />
+        </li>
+        <li>
+          <img src={sliderThumb3} alt="" />
+        </li>
+      </ul>
+
+      <ul className="sci">
+        <li>
+          <a href="#">
+            <img src={instagram} alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={facebook} alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={twitter} alt="" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SliderHomepage;
