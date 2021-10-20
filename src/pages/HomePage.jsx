b224a1096ce768859b13baa212e9ea223afce47a
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2021-10-20 08:47:23 +0700

    restructuring slider styles with css module

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SliderHomepage from "../components/SliderHomePage/SliderHomepage";
+import Footer from "../components/Footer/footer";
 
 const HomePage = () => {
-     return (
-          <>
-               <SliderHomepage />
-          </>
-     )
-}
+  return (
+    <>
+      <SliderHomepage />
+      <Footer />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
