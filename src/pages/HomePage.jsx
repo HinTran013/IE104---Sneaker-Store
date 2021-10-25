591fe8860a99960b9bbe750f1ac0ae48458209f6
Lưu Ngọc Sáng <sangjohn58@gmail.com>
2021-10-25 17:35:30 +0700

    fix conflict

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import SliderHomepage from "../components/SliderHomePage/SliderHomepage";
 // import SliderDealOfTheWeek from "../components/SliderDealOfTheWeek/SliderDealOfTheWeek";
+import CollectionItem from "../components/CollectionItem/CollectionItem";
 import BannerFirst from "../assets/images/banner/banner-homepage-1.jpg";
 import BannerSecond from "../assets/images/banner/banner-homepage-2.jpg";
 import SneakerFirst from "../assets/images/banner/sneaker-homepage-1.jpg";
 import SneakerSecond from "../assets/images/banner/sneaker-homepage-2.jpg";
-import style from "../styles/HomePage.module.css";
 
 const HomePage = () => {
   return (
@@ -17,82 +17,27 @@ const HomePage = () => {
       {/* ... */}
 
       {/* the first collection item */}
-      <div className={style.collectionContainer}>
-        <div className={style.itemSneaker}>
-          <a className={style.itemLink} href="#">
-            <img
-              className={style.itemImgSneaker}
-              src={SneakerFirst}
-              alt="sneaker"
-            />
-            <div className={style.sneakerText}>
-              <h5>Running Shoes</h5>
-              <h2>Nike Just Do It</h2>
-            </div>
-          </a>
-        </div>
-
-        <div className={style.itemBanner}>
-          <a className={style.itemLink} href="#">
-            <img
-              className={style.itemImgBanner}
-              src={BannerFirst}
-              alt="banner"
-            />
-          </a>
-
-          <div className={style.bannerText}>
-            <h2>
-              The Benefits
-              <br />
-              Of Running
-            </h2>
-            <h5>Season Off 30-10%</h5>
-            <a className={style.bannerBtn} href="#">
-              More Info
-            </a>
-          </div>
-        </div>
-      </div>
+      <CollectionItem
+        subTxtSneaker='Running Shoes'
+        mainTxtSneaker='Nike Just Do It'
+        sneakerImg={SneakerFirst}
+        subTxtBanner='Season Off 30-10%'
+        mainTxtBannerAbove='The Benefits'
+        mainTxtBannerUnder='Of Running'
+        bannerImg={BannerFirst}
+      />
 
       {/* the second collection item */}
-      <div className={style.collectionContainer}>
-        <div className={style.itemBanner}>
-          <a className={style.itemLink} href="#">
-            <img
-              className={style.itemImgBanner}
-              src={BannerSecond}
-              alt="banner"
-            />
-          </a>
-
-          <div className={`${style.bannerText} ${style.bannerTextRight}`}>
-            <h5>New Arrivals</h5>
-            <h2>
-              Women Hoodies &
-              <br />
-              Sweatshirts
-            </h2>
-            <a className={style.bannerBtn} href="#">
-              More Info
-            </a>
-          </div>
-        </div>
-
-        <div className={style.itemSneaker}>
-          <a className={style.itemLink} href="#">
-            <img
-              className={style.itemImgSneaker}
-              src={SneakerSecond}
-              alt="sneaker"
-            />
-            <div className={style.sneakerText}>
-              <h5>Best Sellers</h5>
-              <h2>Adidas Shoes</h2>
-            </div>
-          </a>
-        </div>
-      </div>
+      <CollectionItem
+        subTxtSneaker='Best Sellers'
+        mainTxtSneaker='Adidas Shoes'
+        sneakerImg={SneakerSecond}
+        subTxtBanner='New Arrivals'
+        mainTxtBannerAbove='Women Hoodies &'
+        mainTxtBannerUnder='Sweatshirts'
+        bannerImg={BannerSecond}
+        reverse={true}
+      />
     </>
   );
 };
