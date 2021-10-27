5c46e7a8b7bbe95e7eb698684363dfc1ce8c5e45
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2021-10-27 16:15:57 +0700

    almost done deal of the week

--- src/components/SliderDealOfTheWeek/SliderDealOfTheWeek.jsx
+++ src/components/SliderDealOfTheWeek/SliderDealOfTheWeek.jsx
@@ -1,45 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import ProgressBar from "./ProgressBar";
 import DealStyle from "./SliderDealOfTheWeek.module.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Data from "./SliderOfTheWeekData";
 
 const SliderDealOfTheWeek = () => {
+  const [contentState, setContentState] = useState({
+    name: Data[0].name,
+    img: Data[0].img,
+    nameOfShoes: Data[0].nameOfShoes,
+    normalPrice: Data[0].normalPrice,
+    salePrice: Data[0].salePrice,
+    btnText: Data[0].btnText,
+    saleAmount: Data[0].saleAmount,
+    alreadySold: Data[0].alreadySold,
+  });
+
+  const switchSlide = (index) => {
+    setContentState({
+      name: Data[index].name,
+      img: Data[index].img,
+      nameOfShoes: Data[index].nameOfShoes,
+      normalPrice: Data[index].normalPrice,
+      salePrice: Data[index].salePrice,
+      btnText: Data[index].btnText,
+      saleAmount: Data[index].saleAmount,
+      alreadySold: Data[index].alreadySold,
+    });
+  };
+
   return (
     <>
-      <div className={`container-fluid ${DealStyle.container}`}>
+      <div className={`${DealStyle.dealContainer}`}>
         <div className={`${DealStyle.titleContainer}`}>
           <h2>Deal Of The Week</h2>
         </div>
 
-        <div className={`row`}>
-          <div className={`col ${DealStyle.sideTextContainer}`}>
-            <p className={`${DealStyle.sideAboveHeaderText} row`}>
-              Hot Deal Week
-            </p>
-            <h3 className={`${DealStyle.sideHeader} row`}>
-              Crazy Byw Lvl X Pw
+        <div className={`${DealStyle.dealContentContainer}`}>
+          <div className={`${DealStyle.sideTextContainer}`}>
+            <p className={`${DealStyle.sideAboveHeaderText}`}>Hot Deal Week</p>
+            <h3 className={`${DealStyle.sideHeader}`}>
+              {contentState.nameOfShoes}
             </h3>
-            <div className={`${DealStyle.priceContainer} row`}>
-              <div className={`${DealStyle.oldPrice} col`}>$250</div>
-              <div className={`${DealStyle.newPrice} col`}>$200</div>
+            <div className={`${DealStyle.priceContainer}`}>
+              <p className={`${DealStyle.salePrice}`}>
+                {contentState.salePrice}$
+              </p>
+              <p className={`${DealStyle.normalPrice}`}>
+                {contentState.normalPrice}$
+              </p>
             </div>
-            <div className={`${DealStyle.soldAvailableContainer} row`}>
-              <div className={`${DealStyle.soldText} col`}>
-                Already sold: 19
-              </div>
-              <div className={`${DealStyle.AvailableText} col`}>
-                Available: 23
-              </div>
+            <div className={`${DealStyle.soldAvailableContainer}`}>
+              <p className={`${DealStyle.soldText}`}>
+                Already sold: {contentState.alreadySold}
+              </p>
+              <p className={`${DealStyle.AvailableText}`}>
+                Sale amount: {contentState.saleAmount}
+              </p>
             </div>
 
-            <progress value="19" max="42"></progress>
+            <ProgressBar
+              done={Math.floor(
+                (Number(contentState.saleAmount) /
+                  Number(contentState.alreadySold)) *
+                  100
+              )}
+              className={`${DealStyle.progress}`}
+            />
 
-            <p className={`${DealStyle.isExpireText}`}>Still Available</p>
+            {/* <p className={`${DealStyle.isExpireText}`}>Still Available</p> */}
 
-            <a className={`${DealStyle.DealBtn}`}>Shop now</a>
+            <a className={`${DealStyle.dealBtn}`}>Shop now</a>
           </div>
 
-          <div className={`col ${DealStyle.sideImageContainer}`}></div>
+          <div className={`col ${DealStyle.sideImageContainer}`}>
+            <img src={Data[0].img} alt="" />
+          </div>
         </div>
+
+        <ul className={DealStyle.thumb}>
+          {Data.map((currentItem, index) => {
+            return (
+              <li
+                key={index}
+                onClick={() => {
+                  switchSlide(index);
+                }}
+              >
+                <img src={currentItem.img} alt="" />
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
   );
---
+++ src/components/SliderDealOfTheWeek/SliderOfTheWeekData.js
@@ -0,0 +1,38 @@
+import sneaker1 from "../../assets/images/converse.png";
+import sneaker2 from "../../assets/images/converse.png";
+import sneaker3 from "../../assets/images/converse.png";
+
+const data = [
+  {
+    name: "slider1",
+    img: sneaker1,
+    nameOfShoes: "Converse 1",
+    normalPrice: "251",
+    salePrice: "201",
+    btnText: "Shop now 1",
+    saleAmount: 23,
+    alreadySold: 19,
+  },
+  {
+    name: "slider2",
+    img: sneaker2,
+    nameOfShoes: "Converse 2",
+    normalPrice: "252",
+    salePrice: "202",
+    btnText: "Shop now 2",
+    saleAmount: 30,
+    alreadySold: 15,
+  },
+  {
+    name: "slider3",
+    img: sneaker3,
+    nameOfShoes: "Converse 3",
+    normalPrice: "253",
+    salePrice: "203",
+    btnText: "Shop now 3",
+    saleAmount: 24,
+    alreadySold: 11,
+  },
+];
+
+export default data;
