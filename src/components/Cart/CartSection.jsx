b3a02a7edd40e6530d1ede47e1568fcd430f233b
PhamneyuT <koduapass@gmail.com>
2021-11-09 23:37:13 +0700

    changed the size of nike pics

--- src/components/Cart/CartSection.jsx
+++ src/components/Cart/CartSection.jsx
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 
 import style from "./CartSection.module.css"
 import CartTable from './CartTable';
+import CartTotal from './CartTotal';
 
 const CartSection = () => {
     return (
-        <section>
-            <CartTable />
-        </section>
+        <div className={style.cartContainer }>
+            <section className={style.cartSection}>
+                <CartTable />
+                <CartTotal />
+            </section>
+        </div>
     )
 };
 
--- src/components/Cart/CartTable.jsx
+++ src/components/Cart/CartTable.jsx
@@ -7,38 +7,39 @@ import { CartItem } from "./CartTableData.js"
 const CartTable = () =>
 {
     return (
-        <table className={style.cartTable}>
-            <thead>
-                <tr className={style.trHead}>
-                    <th>Product</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Total</th>
-                </tr>
-            </thead>
+        <div>
+            <table className={style.cartTable}>
+                <thead>
+                    <tr className={style.trHead}>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
 
-            <tbody className={style.trProduct}>
-                
-                {CartItem.map((item) => 
-                {
-                    return (
-                        <tr>
-                            <th>
-                                <img src={sneaker} alt="" />
-                                <div className={style.nameProduct}>
-                                    <p>Sneaker</p>
-                                    <p>Detail</p>
-                                    <p>Detail</p>
-                                </div>
-                            </th>
-                            <th className={style.amount}>244</th>
-                            <th className={style.quantity}>244</th>
-                            <th className={style.amount}>244</th>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+                <tbody className={style.trProduct}>
+
+                    {CartItem.map((item) => {
+                        return (
+                            <tr>
+                                <th>
+                                    <img src={sneaker} alt="" />
+                                    <div className={style.nameProduct}>
+                                        <p>Sneaker</p>
+                                        <p>Detail</p>
+                                        <p>Detail</p>
+                                    </div>
+                                </th>
+                                <th className={style.amount}>244</th>
+                                <th className={style.quantity}>244</th>
+                                <th className={style.amount}>244</th>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
