99fee661d183457304ab5a8a911c28636ac4cb0c
PhamneyuT <koduapass@gmail.com>
2021-12-02 16:13:25 +0700

    added aboutus and error

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import ProductDetail from "./pages/ProductDetail";
 import AboutUsPage from "./pages/AboutUsPage";
 import BlogPage from "./pages/BlogPage";
 import CartPage from "./pages/CartPage";
+import Page404 from "./pages/ErrorPage";
 import ContactUs from "./pages/ContactUs";
 
 import Navbar from "./components/Navbar/Navbar";
@@ -25,7 +26,7 @@ function App() {
           <Route exact path="/product" component={ProductPage} />
           <Route exact path="/product/:id" component={ProductDetail} />
           <Route path="/about-us" component={AboutUsPage} />
-          <Route path="/blog" component={BlogPage} />
+          <Route path="/blog" component={Page404} />
           <Route path="/cart" component={CartPage} />
           <Route path="/contact" component={ContactUs} />
         </Switch>
