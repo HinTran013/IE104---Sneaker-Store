0611fa0e525c286d10475d8ef5b329cca3bb1b89
Johnny-Luu <ngocsangluu58@gmail.com>
2021-12-02 10:22:05 +0700

    add modal comment UI

--- client/src/components/ProductDetail/CmtAndRating.jsx
+++ client/src/components/ProductDetail/CmtAndRating.jsx
@@ -1,5 +1,8 @@
 import style from './CmtAndRating.module.css'
+import { useState } from 'react';
 import CommentItem from './CommentItem';
+import Nike1 from '../../assets/images/sneaker-transparent/nike-1.png'
+import ReactStars from "react-rating-stars-component";
 import { Bar } from 'react-chartjs-2';
 import {
      Chart as ChartJS,
@@ -59,7 +62,40 @@ const data = {
 };
 
 function CmtAndRating() {
-  return (
+     const [openModal, setopenModal] = useState(false);
+     const [ratingVote, setRatingVote] = useState(0);
+     const [ratingText, setRatingText] = useState('');
+
+     const ratingChanged = (newRating) => {
+          setRatingVote(newRating);
+
+          switch (newRating) {
+               case 1:
+                    setRatingText('Awful');
+                    break;
+               case 2:
+                    setRatingText('Bad');
+                    break;
+               case 3:
+                    setRatingText('Normal');
+                    break;
+               case 4:
+                    setRatingText('Good');
+                    break;
+               case 5:
+                    setRatingText('Excellent');
+                    break;
+               default:
+                    setRatingText('');
+          }
+     };
+
+     const handleOpenModal = () => {
+          setopenModal(!openModal);
+          setRatingText('');
+     };
+
+     return (
      <div className={style.outerContainer}>
           <h2 className={style.titleText}>Comments and Rating</h2>
           <div className={style.mainContainer}>
@@ -74,7 +110,48 @@ function CmtAndRating() {
                     </div>
                </div>
 
-               <h3 className={style.yourCommentTitle}>Your Comment</h3>
+               <div className={style.yourCommentSection}>
+                    <h3 className={style.yourCommentTitle}>Your Comment</h3>
+                    <button className={style.yourCommentButton} onClick={handleOpenModal}>Leave a comment</button>
+
+                    {openModal && 
+                         <div className={style.yourCommentModal} onClick={handleOpenModal}>
+                              <div className={style.modalContainer} onClick={e => {
+                                   e.preventDefault()
+                                   e.stopPropagation()
+                              }}>
+                                   <h2 className={style.modalHeader}>Leave Your Comment Here!</h2>
+                                   <div className={style.modalContent}>
+                                        <p className={style.modalContentText}>Just public your comment to everyone, let's the others know what do you think about this product. Remember, everyone can see your public information like name, avatar, time you write this comment.</p>
+
+                                        <div className={style.modalMainSection}>
+                                             <div className={style.modalThumb}>
+                                                  <img className={style.modalThumbImg} src={Nike1} alt="thumb" />
+                                                  <h4 className={style.modalThumbName}>Nike Pegasus</h4>
+                                             </div>
+                                             <textarea className={style.modalContentInput} type="text" placeholder="Write something..." />
+                                        </div>
+
+                                        <ReactStars
+                                             classNames={style.modalRating}
+                                             count={5}
+                                             onChange={ratingChanged}
+                                             size={24}
+                                             activeColor="#ffd700"
+                                        />
+
+                                        <p className={style.modalRatingText}>{ratingText}</p>
+
+                                        <div className={style.modalButtonSection}>
+                                             <button className={style.modalButtonCancel} onClick={handleOpenModal}>Cancel</button>
+                                             <button className={style.modalButtonSubmit}>Submit</button>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                    }
+               </div>
+
                <div className={style.commentSection}>
                     <CommentItem />
                </div>
@@ -88,7 +165,7 @@ function CmtAndRating() {
                </div>
           </div>
      </div>
-  );
+     );
 }
 
 export default CmtAndRating;
