e4a8b0db8dcdc9de812d3402f78efaf105016ff5
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2021-12-04 15:05:21 +0700

    done update account info

--- client/src/components/PersonalInfo/MainPartInfo.jsx
+++ client/src/components/PersonalInfo/MainPartInfo.jsx
@@ -1,10 +1,63 @@
 import React, { useState } from "react";
 import style from "./MainPartInfo.module.css";
-import DatePicker from "react-datepicker";
+// import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectCustomer } from "../../features/customerSlice";
+import { update } from "../../features/customerSlice";
+import { updateCustomerAccount } from "../../api/customerAPI";
+import ToastMessage from "../ToastMessage/ToastMessage";
 
 function MainPartInfo() {
-  const [selectedDate, setSelectedDate] = useState(null);
+  const dispatch = useDispatch();
+  const customerState = useSelector(selectCustomer);
+
+  // const [selectedDate, setSelectedDate] = useState(null);
+  const [inputName, setInputName] = useState(customerState.name);
+  const [inputPhoneNumber, setInputPhoneNumber] = useState(customerState.phone);
+  const [inputAddress, setInputAddress] = useState(customerState.address);
+  const [inputGender, setInputGender] = useState(customerState.gender);
+
+  function handleNameChange(e) {
+    setInputName(e.target.value);
+  }
+
+  function handlePhoneNumberChange(e) {
+    setInputPhoneNumber(e.target.value);
+  }
+
+  function handleAddressChange(e) {
+    setInputAddress(e.target.value);
+  }
+
+  function handleGender(e) {
+    setInputGender(e.target.value);
+  }
+
+  function handleSave() {
+    console.log("save");
+
+    if (customerState != null) {
+      dispatch(
+        update({
+          name: inputName,
+          phone: inputPhoneNumber,
+          address: inputAddress,
+          gender: inputGender,
+        })
+      );
+
+      updateCustomerAccount(
+        customerState.id,
+        inputGender,
+        inputAddress,
+        inputName,
+        inputPhoneNumber
+      );
+
+      ToastMessage("success", "Update information successfully!");
+    }
+  }
 
   return (
     <div className={style.container}>
@@ -14,24 +67,41 @@ function MainPartInfo() {
       <table>
         <tr>
           <td className={style.rightAlign}>Email</td>
-          <td>email@gmail.com</td>
+          <td>
+            {customerState != null ? customerState.email : "email@gmail.com"}
+          </td>
         </tr>
         <tr>
           <td className={style.rightAlign}>Full Name</td>
           <td>
-            <input className={style.inputBox} type="text" />
+            <input
+              className={style.inputBox}
+              type="text"
+              value={inputName}
+              onChange={handleNameChange}
+            />
           </td>
         </tr>
         <tr>
           <td className={style.rightAlign}>Phone Number</td>
           <td>
-            <input className={style.inputBox} type="tel" />
+            <input
+              className={style.inputBox}
+              type="tel"
+              value={inputPhoneNumber}
+              onChange={handlePhoneNumberChange}
+            />
           </td>
         </tr>
         <tr>
           <td className={style.rightAlign}>Address</td>
           <td>
-            <input className={style.inputBox} type="text" />
+            <input
+              className={style.inputBox}
+              type="text"
+              value={inputAddress}
+              onChange={handleAddressChange}
+            />
           </td>
         </tr>
         <tr>
@@ -41,26 +111,23 @@ function MainPartInfo() {
               className="w3-radio"
               type="radio"
               name="gender"
-              value="male"
+              value="true"
+              onChange={handleGender}
+              checked={inputGender === "true" ? true : null}
             />
             <label> Male </label>
             <input
               className="w3-radio"
               type="radio"
               name="gender"
-              value="female"
+              value="false"
+              onChange={handleGender}
+              checked={inputGender === "false" ? true : null}
             />
             <label> Female </label>
-            <input
-              className="w3-radio"
-              type="radio"
-              name="gender"
-              value="other"
-            />
-            <label> Other </label>
           </td>
         </tr>
-        <tr>
+        {/* <tr>
           <td className={style.rightAlign}>Day of Birth</td>
           <td>
             <DatePicker
@@ -70,11 +137,13 @@ function MainPartInfo() {
               dateFormat="dd/MM/yyyy"
             />
           </td>
-        </tr>
+        </tr> */}
         <tr>
           <td></td>
           <td>
-            <div className={style.button}>Save</div>
+            <div onClick={handleSave} className={style.button}>
+              Save
+            </div>
           </td>
         </tr>
       </table>
--- client/src/features/customerSlice.js
+++ client/src/features/customerSlice.js
@@ -1,21 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const customerSlice = createSlice({
-     name: 'customer',
-     initialState: {
-          customer: null
-     },
-     reducers: {
-          login: (state, action) => {
-               state.customer = action.payload;
-          },
+  name: "customer",
+  initialState: {
+    customer: null,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.customer = action.payload;
+    },
 
-          logout: (state) => {
-               state.customer = null;
-          }
-     }
+    logout: (state) => {
+      state.customer = null;
+    },
+
+    update: (state, action) => {
+      state.customer.name = action.payload.name;
+      state.customer.gender = action.payload.gender;
+      state.customer.address = action.payload.address;
+      state.customer.phone = action.payload.phone;
+    },
+  },
 });
 
-export const { login, logout } = customerSlice.actions;
-export const selectCustomer = state => state.customer.customer;
+export const { login, logout, update } = customerSlice.actions;
+export const selectCustomer = (state) => state.customer.customer;
 export default customerSlice.reducer;
